19ebd5f9784544fd60b9d5747564c42d4b47bd27
NamND <dinhnam1193@gmail.com>
2020-05-28 22:22:57 +0700

    Nam Adding new file Car*.h and Car*.c

---
+++ Car.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Car
+{
+private:
+	
+public:
+
+};
+
---
+++ CaroSafe.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class CaroSafe
+{
+private:
+	int speedRange[4] = { 0 }; // Mang chua cac moc toc do quy dinh khoang cach
+	int arr_numbID[8] = { 0 }; // MANG chua ma so ca nhan 8 chu so
+public:
+
+};
+
